1c33f8312ad060a655bf2a4058ca2d7461d68b17
SzymonRem <126695909+SzymonRem@users.noreply.github.com>
2023-05-24 15:11:43 +0000

    Zaliczenie

--- Main.java
+++ Main.java
@@ -1,32 +1,27 @@
 import java.util.Scanner;
 
-
 class Main {
-  public static void main(String[] args) {
-     Scanner scan = new Scanner(System.in);
-    System.out.println("1.Zamaian PLN na EUR");
-    System.out.println("2.Zamaian EUR na PLN");
-    System.out.println("Podaj opcje:");
-    int opcja=scan.nextInt();
-    switch(opcja){
-      case 1:
-        {
-           System.out.println("Podaj ilość złotych:");
-          double zlote =scan.nextDouble();
-          double wynik=0;
-          double przelicznik = 0.22;
-          wynik = zlote*przelicznik;
-          System.out.println(zlote+" "+"to"+wynik+"Euro");
-          
+public static void main(String[] args) {
+Scanner scan = new Scanner(System.in);
+System.out.println("Podaj wyraz");
+String wyraz = scan.nextLine();
+char[] Wyraz = wyraz.toCharArray();
+char[] Samogłoski ={'a','ą','e','ę','i','o','u','y','ó','A','Ą','E','Ę','I','O','U','Y','Ó'};
+
+for(int i=0;i<wyraz.length();i++){
+        for(int j=0;j<Samogłoski.length;j++){
+          if(Wyraz[i] == Samogłoski[j]){
+            System.out.println("Samaogłoski"+" "+ Wyraz[i]);
+            break;
+          }
+          else{
+             System.out.println("Spółgłoski"+" "+Wyraz[i]);
+            break;
+          }
         }
-      case 2:{
-          System.out.println("Podaj ilość euro:");
-        double euro=scan.nextDouble();
-        double przelicznik2 = 4.50;
-        double wynik = euro*przelicznik2;
-         System.out.println(euro+" "+"to"+" "+wynik+" "+"Złotych");
-      }
-    }
-    
-  }
 }
+
+}
+}
+
+
